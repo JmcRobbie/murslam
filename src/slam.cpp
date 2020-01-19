c6d1a1ab7c37ed4dfdbe19da48c2b20f76ddbf83
Jack McRobbie <jackmcrobbie@hotmail.com>
2020-01-20 02:04:08 +1100

    Implemented launch file, and subscriber publisher nodes

--- src/slam.cpp
+++ src/slam.cpp
@@ -1,30 +1,44 @@
 #include "ros/ros.h"
-
+#include <fssim_common/State.h>
 #include <fssim_common/Track.h>
 #include <sstream>
 #include "std_msgs/String.h"
+#include "sensor_msgs/PointCloud2.h"
 
 #define rate 1000
 
+void basiccallback(const sensor_msgs::PointCloud2::ConstPtr& msg);
+
 int main(int argc, char **argv){
     ros::init(argc, argv, "slam");
-    ros::NodeHandle n;
 
-    ros::Publisher chatter_pub = n.advertise<fssim_common::Track>("slam", rate);
+    ros::NodeHandle n;
+    /* Publishers*/
+
+    ros::Publisher slam_pub = n.advertise<fssim_common::Track>("slam", rate);
+    ros::Publisher talker = n.advertise<std_msgs::String>("talker", rate);
+
+    /*Listeners*/
+
+    ros::Subscriber lidar_subs = n.subscribe("/lidar/cones",rate,basiccallback);
+    ros::Subscriber camera_sub = n.subscribe("/camera/cones",rate,basiccallback);
 
     ros::Rate loop_rate(10);
-
     int count = 0;
-
     while(ros::ok()){
       std_msgs::String msg;
       std::stringstream ss;
       ss<<"Hello world"<<count;
       msg.data = ss.str();
       ROS_INFO("%s", msg.data.c_str());
-      chatter_pub.publish(msg);
+      talker.publish(msg);
       ros::spinOnce();
       loop_rate.sleep();
       ++count;
     }
+}
+
+void basiccallback(const sensor_msgs::PointCloud2::ConstPtr& msg){
+
+/* Placeholder function, will be replaced later.*/
 }
