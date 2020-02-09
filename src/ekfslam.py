5cd3a66135c05129fc712d35279541a90578b907
Jack McRobbie <jackmcrobbie@hotmail.com>
2020-02-09 23:18:54 +1100

    asdf

--- src/ekfslam.py
+++ src/ekfslam.py
@@ -4,7 +4,7 @@ import rospy
 '''
 EKF slam implementation for MUR driverless
 
-Author: Jack McRobbie 
+Author: Jack McRobbie
 '''
 import numpy as np
 import rospy
@@ -20,6 +20,7 @@ class EkfSlam:
         self.dt = 1.0/self.rate
         self.stateSize = 3  # [x y yaw]
         self.coneStateSize = 2
+        self.minDistThresh = 1  # Minumum distance threshold
 
         # EKF state variance
         self.Cx = np.diag([0.5, 0.5, np.deg2rad(30.0)]) ** 2
@@ -30,13 +31,13 @@ class EkfSlam:
         '''
         Main ekfslam loop, will run until the node is terminated.
         '''
+        rospy.loginfo("Doing ekf slam at %.0f HZ \n", self.rate)
 
         while not rospy.is_shutdown():
             rate = rospy.Rate(self.rate)
-            rospy.loginfo("Doing ekf slam at 10 HZ \n")
             rate.sleep()
 
-    def ekfSlam(self, X_est, Pest):
+    def ekfSlam(self, X_est, Pest, cam_obs, lidar_obs):
         # Predict
         u = self.sensors.driveCmd
         X_est = self.motionModel(
@@ -49,9 +50,28 @@ class EkfSlam:
                                                              Pest[0:self.stateSize,
                                                                   0:self.stateSize],
                                                              G) + np.matmul(Fx.T, self.Cx, Fx)
-        # Update
-        for coneOb in range(2):
+        # Update ignoring camera based observations for now
+        for coneOb in range(len(lidar_obs)):
+            min_id = self.search_correspond_landmark_id(
+                X_est, Pest, coneOb[0:2])
             num_cones = self.calcNumCones(X_est)
+            if min_id == num_cones:
+                ros.loginfo(
+                    "New cone identified \nExtending covariance matrix")
+                Xtmp = np.vstack(
+                    (X_est, cam_obs))
+                Ptmp = np.vstack((np.hstack((Pest, np.zeros((len(X_est), 2)))),
+                                  np.hstack((np.zeros((2, len(X_est))), initP))))
+                X_est = Xtmp
+                Pest = Ptmp
+            lm = get_landmark_position_from_state(X_est, min_id)
+            y, s, H = calc_innovation(lm, X_est, Pest, coneOb, min_id)
+            K = np.matmul(Pest, H.T, np.linalg.inv(s))
+            X_est = X_est + np.matmul(K, y)
+            Pest = np.eye(len(X_est)) - np.matmul(K, H, Pest)
+
+        X_est[2] = pi_2_pi(X_est[2])
+        return X_est, Pest
 
     def motionModel(self, x, u):
         F = np.array([[1.0, 0, 0],
@@ -80,3 +100,23 @@ class EkfSlam:
     def calcNumCones(self, x):
         n = int((len(x) - self.stateSize) / self.coneStateSize)
         return n
+
+    def search_correspond_landmark_id(self, X_est, P_est, landmark):
+        """
+        Landmark association with Mahalanobis distance
+        """
+
+        nLM = self.calcNumCones(X_est)
+
+        min_dist = []
+
+        for i in range(nLM):
+            lm = self.get_landmark_position_from_state(X_est, i)
+            y, S, H = self.calc_innovation(lm, X_est, P_est, landmark, i)
+            min_dist.append(np.matmul(y.T, np.linalg.inv(S), y))
+
+        min_dist.append(self.minDistThresh)  # new landmark
+
+        min_id = min_dist.index(min(min_dist))
+
+        return min_id
